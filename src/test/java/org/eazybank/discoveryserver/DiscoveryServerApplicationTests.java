21b8c92fbc8fbf7950f4f69a0be42d086045c8cc
dennis githinji <wakahiad@gmail.com>
2024-05-19 11:50:22 +0300

    disovery server setup

---
+++ src/test/java/org/eazybank/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eazybank.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
